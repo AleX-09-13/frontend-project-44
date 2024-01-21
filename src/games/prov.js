f2ca17cb39ab0f0d57e8bedc391731b1098fd4dd
Alex <silkov389@gmail.com>
2024-01-21 14:32:54 +0300

    12.5

--- src/games/prov.js
+++ src/games/prov.js
@@ -1,16 +1,14 @@
-import readlineSync from 'readline-sync';
-import { hi } from '../index.js';
-const userName = hi();
-console.log('What number is missing in the progression?');
-
 // Функция для создания рандомной арифметической прогрессии с одним скрытым элементом и рандомной позицией
 const generateRandomArithmeticProgressionWithHiddenElement = () => {
   const length = 7; // Колличество цифр в прогрессии
   const hiddenIndex = Math.floor(Math.random() * length); // выбираем рандомную позицию для скрытого элемента
   let progression = [];
+
   let definableNumber;
+
   const start = Math.floor(Math.random() * 10);
   const diff = Math.floor(Math.random() * 5) + 1;
+
   for (let i = 1; i <= length; i++) {
     if (i === hiddenIndex + 1) {
       progression.push('..'); // элемент скрыт
@@ -21,36 +19,8 @@ const generateRandomArithmeticProgressionWithHiddenElement = () => {
     }
   }
   let progression2 = progression.join(' '); // заменяем запятые на пробелы
+  console.log(progression2);
   return { progression2, definableNumber };
 };
-
+generateRandomArithmeticProgressionWithHiddenElement();
 // Завершение функции
-// Генерация одной рандомной арифметической прогрессии
-let count = 0; // Счетчик правильных ответов
-// Функция главная
-export const brainPr = () => {
-  if (count >= 3) {
-    console.log(`Congratulations, ${userName}!`);
-    return;
-  }
-
-  const prog = generateRandomArithmeticProgressionWithHiddenElement();
-  const progression = prog.progression2;
-  const correctAnswer = prog.definableNumber;
-  console.log(`Question: ${progression}`);
-  const userAnswer = readlineSync.question('Your answer: ');
-  if (parseInt(userAnswer) === correctAnswer) {
-    console.log('Correct!');
-    count++;
-    brainPr();
-  } else {
-    return (
-      console.log(`Question: ${progression}`),
-      console.log(`Your answer: ${userAnswer}`),
-      console.log(
-        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`
-      ),
-      console.log(`Let's try again, ${userName}!`)
-    );
-  }
-};
