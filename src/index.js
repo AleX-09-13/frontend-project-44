352b1da020466af9bb8c50cf8253073f12b1a907
Alex <silkov389@gmail.com>
2024-02-29 16:03:25 +0300

    18.4

--- src/index.js
+++ src/index.js
@@ -9,21 +9,20 @@ const driver = (runGame, question) => {
   while (count < 3) {
     const gameResult2 = runGame();
     const { rightAnswer } = gameResult2;
-    if (count >= 3) {
-      console.log(`Congratulations, ${userName}!`);
-      break;
-    }
     console.log(`Question: ${gameResult2.questionTwo}`);
     const yourAnswer = readlineSync.question('Your answer: ');
     if (rightAnswer === yourAnswer) {
       console.log('Correct!');
       count += 1;
     } else {
-      console.log(`your_answer is wrong answer ;(. Correct answer "${rightAnswer}".`);
+      console.log(`Your answer is a wrong answer ;(. Correct answer "${rightAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
       break;
     }
   }
-  console.log(`Let's try again, ${userName}!`);
+  if (count === 3) {
+    console.log(`Congratulations, ${userName}!`);
+  }
   return userName;
 };
 
