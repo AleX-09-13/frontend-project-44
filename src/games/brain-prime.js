f6da7f335f0a1f999a793803bffefb342ffdebda
Alex <silkov389@gmail.com>
2024-03-04 21:42:11 +0300

    18.7

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,7 +2,7 @@ import driver from '../index.js';
 import getRandomNumber from './RandomNumber.js';
 
 function isPrime(num) {
-  for (let i = 2; i < num; i++) {
+  for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return false;
   }
   return num !== 1;
