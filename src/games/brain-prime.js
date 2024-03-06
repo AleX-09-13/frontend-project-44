4ad437a8e046a7b4e3fffb3fa0f46945c140eb27
Alex <silkov389@gmail.com>
2024-03-06 11:12:48 +0300

    18.9

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,10 +2,11 @@ import driver from '../index.js';
 import getRandomNumber from './RandomNumber.js';
 
 function isPrime(num) {
-  for (let i = 2; i < num; i += 1) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
-  return num !== 1;
+  return true;
 }
 const question = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const runGame = () => {
